c9cf16d1c343bdc76e6ad1bab50d72c5608c9211
Terry Filiba <tfiliba@gmail.com>
2011-12-15 15:12:26 -0800

    Adding table of results

--- lwa_hist/gen_memo.py
+++ lwa_hist/gen_memo.py
@@ -6,10 +6,12 @@ import matplotlib
 matplotlib.use('Agg')
 import pylab
 from os.path import basename,splitext
+import sympy.galgebra.latex_ex as tex
 
 bins=numpy.arange(-2048,2048,1)
 
-texfile = open('plots.tex','w')
+imagefile = open('plots.tex','w')
+tablefile = open('table.tex','w')
 figuretext = '\\begin{figure}[ht] \
 				\\subfloat{\\includegraphics[width=0.5\\textwidth]{%s}} \
 				\\subfloat{\\includegraphics[width=0.5\\textwidth]{%s}} \
@@ -18,33 +20,40 @@ figuretext = '\\begin{figure}[ht] \
 				
 figurecaption = 'Data from %s. RMS is %f Samples used : %d. If the RMS is unchanged %f percent of the samples will lie within [-128,128). \
  				 With an RMS of 20 %f percent of the samples will lie within [-128,128).'
+ 				 
+resultsarray = numpy.array(['\\text{Stand}','\\text{RMS}','\\text{samples }\in [-128,128)','\\text{samples }\in [-128,128)\\text{ with RMS}=20'])
 
 for file in glob.glob('data/*.npy'):
-	currenthist=numpy.load(file)
-	
-	figurename = 'plots/' + splitext(basename(file))[0] + '.png'
-	logfigurename = 'plots/' + splitext(basename(file))[0] + '_log.png'
-	
-	pylab.clf()
-	
-	# pylab.plot(bins,currenthist/numpy.sum(currenthist),marker='.',linestyle='None')
-	# pylab.xlabel('Power level')
-	# pylab.ylabel('Frequency')
-	# pylab.savefig(figurename,format='png')
-	# #display a log plot
-	# pylab.yscale('log')
-	# pylab.savefig(logfigurename,format='png')
-	
-	
-	rms = numpy.sqrt(1/numpy.sum(currenthist) * numpy.sum(numpy.square(bins)*currenthist) )
-	percentunchwithin8bits = numpy.sum(currenthist[2048-128:2048+128])/numpy.sum(currenthist)*100
-	percentrms20withing8bits = numpy.sum(currenthist[int(2048-128*rms/20)+1:int(2048+128*rms/20)])/numpy.sum(currenthist)*100
+    currenthist=numpy.load(file)
 
-	thisfigurecaption = figurecaption % (splitext(basename(file))[0].strip('hist'), rms, numpy.sum(currenthist),percentunchwithin8bits, percentrms20withing8bits)
-	
-	texfile.write(figuretext % (figurename,logfigurename,thisfigurecaption,) )
+    figurename = 'plots/' + splitext(basename(file))[0] + '.png'
+    logfigurename = 'plots/' + splitext(basename(file))[0] + '_log.png'
 
-texfile.close()
+    pylab.clf()
+
+    # pylab.plot(bins,currenthist/numpy.sum(currenthist),marker='.',linestyle='None')
+    # pylab.xlabel('Power level')
+    # pylab.ylabel('Frequency')
+    # pylab.savefig(figurename,format='png')
+    # #display a log plot
+    # pylab.yscale('log')
+    # pylab.savefig(logfigurename,format='png')
+
+    rms = numpy.sqrt(1/numpy.sum(currenthist) * numpy.sum(numpy.square(bins)*currenthist) )
+    percentunchwithin8bits = numpy.sum(currenthist[2048-128:2048+128])/numpy.sum(currenthist)*100
+    percentrms20withing8bits = numpy.sum(currenthist[int(2048-128*rms/20)+1:int(2048+128*rms/20)])/numpy.sum(currenthist)*100
+
+    thisfigurecaption = figurecaption % (splitext(basename(file))[0].strip('hist'), rms, numpy.sum(currenthist),percentunchwithin8bits, percentrms20withing8bits)
+
+    imagefile.write(figuretext % (figurename,logfigurename,thisfigurecaption,) )
+
+    resultsarray = numpy.vstack([resultsarray,(splitext(basename(file))[0].strip('hist'),rms, percentunchwithin8bits, percentrms20withing8bits)])
+
+
+tablefile.write(tex.LaTeX(resultsarray,inline=True).replace('#','').replace('cccc','c||c|c|c').replace('\\left [','').replace('\\right ]',''))
+
+imagefile.close()
+tablefile.close()
 
 
 
